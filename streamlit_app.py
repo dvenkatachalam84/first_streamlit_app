d7b4a70930ddd0443bd28e4fed02cfb8542396bb
dvenkatachalam84 <63552392+dvenkatachalam84@users.noreply.github.com>
2022-04-14 17:57:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Kale, Spinach, rocket healthy')
+streamlit.text('Avacado toast')
+streamlit.header('Build your own Smootie')
+
