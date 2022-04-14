d8bdd606326b558fc5cc95957f515d1e4336b12d
dvenkatachalam84 <63552392+dvenkatachalam84@users.noreply.github.com>
2022-04-14 18:00:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Kale, Spinach, rocket healthy')
 streamlit.text('Avacado toast')
 streamlit.header('Build your own Smootie')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
